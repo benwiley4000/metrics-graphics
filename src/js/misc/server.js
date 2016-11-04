62cb54af5d7c1a91dd76492bc258548aa11d61a7
Ben Wiley <therealbenwiley@gmail.com>
2016-11-04 15:36:02 -0400

    Sync virtual rendering functions included.

--- src/js/misc/server.js
+++ src/js/misc/server.js
@@ -58,5 +58,60 @@ function use_virtual_element(fn, jsdom_instance) {
   });
 }
 
+function find_virtual_window_sync(MG, jsdom) {
+  if (MG.virtual_window) {
+    return MG.virtual_window;
+  }
+
+  var doc = jsdom.jsdom({
+    html: '',
+    features: { QuerySelector: true }
+  });
+  var virtual_window = doc.defaultView;
+  MG.virtual_window = virtual_window;
+  return virtual_window;
+}
+
+function use_virtual_element_sync(fn, jsdom_instance) {
+  var global_scope = typeof window === 'undefined' ? global : window;
+
+  var jsdom = jsdom_instance || global_scope.jsdom;
+  if (typeof jsdom === 'undefined') {
+    throw 'jsdom could not be found.';
+  }
+
+  var virtual_window = find_virtual_window_sync(MG, jsdom);
+  var virtual_d3 = d3.select(virtual_window.document);
+  var virtual_element_id = 'elem-' + MG.virtual_element_id++;
+  virtual_d3.select('body').append('div').attr('id', virtual_element_id);
+
+  var original_d3 = global_scope.d3;
+  var original_window = global_scope.window;
+  var original_document = global_scope.document;
+  global_scope.d3 = virtual_d3;
+  global_scope.window = virtual_window;
+  global_scope.document = virtual_window.document;
+
+  var target = '#' + virtual_element_id;
+  var error;
+  try {
+    fn(target);
+  } catch(e) {
+    error = e;
+  }
+
+  global_scope.d3 = original_d3;
+  global_scope.window = original_window;
+  global_scope.document = original_document;
+
+  if (error) {
+    throw error;
+  }
+  var markup = virtual_d3.select(target).html();
+  virtual_d3.select(target).remove();
+  return markup;
+}
+
 MG.virtual_element_id = 0;
 MG.use_virtual_element = use_virtual_element;
+MG.use_virtual_element_sync = use_virtual_element_sync;
